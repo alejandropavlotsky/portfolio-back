a07fb8bb123e57b3974e34d9b22e94a32729d554
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2024-09-12 16:32:21 +0200

    FIX

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const app = express();
 app.use(cors())
 app.use(express.json())
 
 app.use(express.static('dist'))
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        // Bloqueo por defecto
+        defaultSrc: ["'none'"],
+        // Permitir la carga de imágenes desde el servidor propio (self)
+        imgSrc: ["'self'", 'https://portfolio-alejandro-murawczik.onrender.com'],
+        // Otros recursos como scripts o estilos se pueden configurar aquí
+        scriptSrc: ["'self'"],
+        styleSrc: ["'self'"],
+        // Permitir conexiones de fuentes (en caso de que uses APIs)
+        connectSrc: ["'self'"],
+      },
+    },
+  })
+);
 
 const projects = require('./projects.json');
 
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/index.html');
+  response.send('<h1>Hello World</h1>');
 })
 
 app.get('/api/projects', (request, response) => {
