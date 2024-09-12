ec0d64d8bd7956e1cee44c6b8545b40230ddb6f1
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2024-09-12 15:37:52 +0200

    CHANGE path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-
+const path = require('path');
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3001
 const projects = require('./projects.json');
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.sendFile(path.join(__dirname, 'dist/index.html'));
 })
 app.get('/api/projects', (req, res) => {
   res.json(projects);
