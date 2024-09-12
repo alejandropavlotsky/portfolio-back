820d7d7e20737db2d0fcce61a875f10e3663e9a1
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2024-09-12 16:45:31 +0200

    FIX

--- index.js
+++ index.js
@@ -2,27 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.static('dist'));
 
 const projects = require('./projects.json');
 
-app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/projects', (request, response) => {
   response.json(projects);
 });
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint);
 
 const port = 3001;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
